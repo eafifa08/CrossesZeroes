7342fc7fba1924afc7d0b2d1cc3e9973ca20ae9d
eafifa08 <eafifa07@yandex.ru>
2021-03-11 17:56:37 +0500

    1

--- Logic.py
+++ Logic.py
@@ -1,21 +1,28 @@
 import random
 
 
-class Field:
-    size = width, height = 3, 3
-    def __init__(self):
-        # '9' - is empty cell
-        # second comment
-        # toghe comment
-        cells = [[9, 9, 9], [9, 9, 9], [9, 9, 9]]
-
-
-
 class Player:
     def __init__(self, symbol):
         self.name = 'Player' + str(random.randint(1, 100))
         self.symbol = symbol
 
 
+class Field:
+    size = width, height = 3, 3
+    def __init__(self):
+        # '9' - is empty cell
+        cells = [[9, 9, 9], [9, 9, 9], [9, 9, 9]]
+
+
 class GameProcess:
-    pass
+    def __init__(self):
+        self.playerA = Player()
+        self.playerB = Player()
+        self.field = Field()
+
+
+    def start(self):
+        pass
+
+
+
--- main.py
+++ main.py
@@ -3,3 +3,4 @@ player_A = Logic.Player(0)
 print('created', player_A.name, 'with symbol ', player_A.symbol)
 player_B = Logic.Player(1)
 print('created', player_B.name, 'with symbol ', player_B.symbol)
+
