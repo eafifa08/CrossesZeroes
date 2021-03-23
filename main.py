22449419481a4d9e64293eb46defdb527279cb82
eafifa08 <eafifa07@yandex.ru>
2021-03-23 16:06:16 +0500

    pygame field

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ import pygame
 import sys
 
 pygame.init()
+color_grey = (100, 100, 100)
+color_red = (250, 0, 0)
 
 screen = pygame.display.set_mode((310, 310))
 r = pygame.Rect(5, 5, 300, 300)
-pygame.draw.rect(screen, (100, 100, 100), r, 0)
-#line = pygame.draw.line()
+pygame.draw.rect(screen, color_grey, r, 0)
+line = pygame.draw.line(screen, color_red, [105, 5], [105, 305])
+line = pygame.draw.line(screen, color_red, [205, 5], [205, 305])
+line = pygame.draw.line(screen, color_red, [5, 105], [305, 105])
+line = pygame.draw.line(screen, color_red, [5, 205], [305, 205])
+
+
+pygame.display.flip()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    pygame.display.flip()
+
 
 player_A = Logic.Player(0)
 print('created', player_A.name, 'with symbol ', player_A.symbol)
