b30cf2a2316d4a23d0de577ec127f1c32d5eb4db
eafifa08 <eafifa07@yandex.ru>
2021-03-23 17:54:28 +0500

    pygame field

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import pygame
 import sys
 
 pygame.init()
+pygame.display.set_caption('Crosses & NuLLs')
+
 color_grey = (100, 100, 100)
 color_red = (250, 0, 0)
 
 screen = pygame.display.set_mode((310, 310))
+
 r = pygame.Rect(5, 5, 300, 300)
 pygame.draw.rect(screen, color_grey, r, 0)
 line = pygame.draw.line(screen, color_red, [105, 5], [105, 305])
 line = pygame.draw.line(screen, color_red, [205, 5], [205, 305])
 line = pygame.draw.line(screen, color_red, [5, 105], [305, 105])
 line = pygame.draw.line(screen, color_red, [5, 205], [305, 205])
-
+font = pygame.font.SysFont('couriernew', 20)
+text = font.render(str('HELLO'), True, pygame.color.THECOLORS['green'])
+screen.blit(text, (50, 50))
 
 pygame.display.flip()
 
@@ -22,6 +27,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print('pressed', pygame.mouse.get_pos())
+            pygame.draw.circle(screen, color_red, pygame.mouse.get_pos(), 2, 2)
+
+            pygame.display.flip()
 
 
 player_A = Logic.Player(0)
@@ -29,7 +39,6 @@ print('created', player_A.name, 'with symbol ', player_A.symbol)
 player_B = Logic.Player(1)
 print('created', player_B.name, 'with symbol ', player_B.symbol)
 field = Logic.Field()
-
 # is_the_end=9 - Победителя нет, есть свободные клетки
 # is_the_end=0 - Победитель с символом 0
 # is_the_end=1 - Победитель с символом 1
@@ -47,3 +56,4 @@ while(is_the_end == 9):
         print(field.cells[1])
         print(field.cells[2])
     is_the_end = field.check_the_winner()
+
